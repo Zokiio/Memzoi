cdf40bdea0c6acb82d93dc05e8725c5ed787c2c6
Joakim Hall <joakim.hall1@gmail.com>
2026-07-15 20:39:12 +0200

    Normalize proposal packet imports

--- crates/memzoi-core/src/service/proposal_packets/mod.rs
+++ crates/memzoi-core/src/service/proposal_packets/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 use super::canonical_write::{
     CanonicalFileWrite, StagedCanonicalFileWrite, cleanup_staged_canonical_writes,
     finalize_staged_canonical_writes, install_staged_canonical_writes,
@@ -112,4 +114,3 @@ impl<'a> ProposalPacketLifecycle<'a> {
         preflight_pending_proposal_root(self.paths)
     }
 }
-use std::collections::BTreeSet;
